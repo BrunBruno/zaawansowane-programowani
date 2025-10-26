1ea0a971b39f5fc53ef1a1696cb0b2b86a534954
KOMPUTR\bruno <brunomaslon@gmail.com>
2025-10-26 14:57:49 +0100

    html

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("test")
+
+day = "Monday"
+
+# Match the day to predefined patterns
+match day:
+    case "Saturday" | "Sunday":
+        print(f"{day} is a weekend.")  # Match weekends
+    case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+        print(f"{day} is a weekday.")  # Match weekdays
+    case _:
+        print("That's not a valid day of the week.")  # Default case
